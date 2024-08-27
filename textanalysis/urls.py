d64f208e501f76091cef3f6888c027c9aa30171d
TriptiSingh743 <singhtripti743@gmail.com>
2024-08-27 12:09:08 +0530

    update

--- textanalysis/urls.py
+++ textanalysis/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from textanalysis.views import home, document_type_selection, upload_page, upload_image, entities
+from textanalysis.views import home
 from . import views
 
 from django.urls import path
@@ -8,10 +8,10 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),  # This should be the default route
-    path('document-type-selection/', document_type_selection, name='document_type_selection'),
-    path('upload-page/', upload_page, name='upload_page'),
-    path('api/upload-image/', upload_image, name='upload_image'),
-    path('entities/', entities, name='entities'),
+    path('document-type-selection/', views.document_type_selection, name='document_type_selection'),
+    path('upload-page/', views.upload_page, name='upload_page'),
+    path('api/upload-image/', views.upload_image, name='upload_image'),
+    path('entities/', views.entities, name='entities'),
     path('contact/', views.contact_view, name='contact'),
     path('success/', views.success_view, name='success'),
     
