598334a4072fb507ea7a7204015ac5d56d4c00ad
Tripti Singh <131510415+TriptiSingh743@users.noreply.github.com>
2024-08-27 12:12:00 +0530

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -2,10 +2,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('textanalysis.urls')),  # Ensure this points to your app's urls.py
+    path('', include('textanalysis.urls')), 
 ]
 
 urlpatterns += staticfiles_urlpatterns()
-
